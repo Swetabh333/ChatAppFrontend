1a3b0245b28a957e8ef29d929a5bc83bea571052
Swetabh333 <swetabh333@gmail.com>
2024-05-30 00:57:05 +0530

    backendUrl

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const backendURL =
-  process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/";
+const backendURL = process.env.REACT_APP_BACKEND_URL;
 
 const axiosInstance = axios.create({
   baseURL: backendURL,
